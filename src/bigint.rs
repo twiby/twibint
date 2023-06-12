e0cba469178422f26d9aecbd386efe7a6d6f7f9a
twiby <t.balenbois@gmail.com>
2023-06-12 10:34:43 +0200

    BigInt: implement Display

--- src/bigint.rs
+++ src/bigint.rs
@@ -114,3 +114,9 @@ impl Ord for BigInt {
         Ordering::Equal
     }
 }
+
+impl std::fmt::Display for BigInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", String::from(self))
+    }
+}
