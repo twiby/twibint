66751804f1645260927498fcaebf072b688e8714
twiby <t.balenbois@gmail.com>
2023-06-12 10:13:02 +0200

    BigInt: implement full division and rem

--- src/ops.rs
+++ src/ops.rs
@@ -295,6 +295,8 @@ impl MulAssign<u32> for BigInt {
 
         if c1 > 0 {
             self.val.push(c1);
+        } else {
+            self.remove_trailing_zeros();
         }
     }
 }
@@ -426,5 +428,104 @@ impl Div<&BigInt> for u32 {
 impl DivAssign<&BigInt> for u32 {
     fn div_assign(&mut self, other: &BigInt) {
         *self = *self / other;
+    }
+}
+impl Rem<&BigInt> for u32 {
+    type Output = u32;
+    fn rem(self, other: &BigInt) -> u32 {
+        if other == &BigInt::new(0) {
+            panic!("Attempt at division by zero.");
+        }
+
+        let big_self = BigInt::new(self);
+        match big_self.cmp(other) {
+            Ordering::Equal => 0u32,
+            Ordering::Less => self,
+            Ordering::Greater => big_self.val[0] % other.val[0],
+        }
+    }
+}
+impl RemAssign<&BigInt> for u32 {
+    fn rem_assign(&mut self, other: &BigInt) {
+        *self = *self / other;
+    }
+}
+
+impl RemDiv<BigInt> for BigInt {
+    type DivOutput = BigInt;
+    type RemOutput = BigInt;
+    fn rem_div(&self, other: &BigInt) -> Option<(BigInt, BigInt)> {
+        if other == &BigInt::new(0) {
+            return None;
+        }
+
+        match self.cmp(other) {
+            Ordering::Equal => return Some((BigInt::new(1), BigInt::new(0))),
+            Ordering::Less => return Some((BigInt::new(0), self.clone())),
+            _ => (),
+        }
+
+        if self.val.len() == 1 {
+            return Some((
+                BigInt::new(self.val[0] / other),
+                BigInt::new(self.val[0] % other),
+            ));
+        }
+
+        assert!(self.val.len() >= other.val.len());
+
+        let mut ret = BigInt::new(0);
+        let mut remainder = BigInt::new(0);
+        for idx in (0..self.val.len()).rev() {
+            remainder = &(remainder << 32) ^ &BigInt::new(self.val[idx]);
+
+            match remainder.cmp(other) {
+                Ordering::Less => continue,
+                Ordering::Equal => {
+                    remainder -= other;
+                    ret += BigInt::new(1) << 32 * idx;
+                }
+                Ordering::Greater => {
+                    let mut quotient = 0u32;
+                    let mut product = BigInt::new(0);
+
+                    // We add to the current product power of 2 by power of 2
+                    for bit in (0..32).rev() {
+                        let temp = (1 << bit) * other;
+                        if &product + &temp <= remainder {
+                            quotient += 1 << bit;
+                            product += temp;
+                        }
+                    }
+
+                    remainder -= &product;
+                    ret += BigInt::new(quotient) << 32 * idx;
+                }
+            };
+        }
+
+        Some((ret, remainder))
+    }
+}
+impl RemAssign<&BigInt> for BigInt {
+    fn rem_assign(&mut self, other: &BigInt) {
+        *self = &*self % other;
+    }
+}
+impl Rem<&BigInt> for &BigInt {
+    type Output = BigInt;
+    fn rem(self, other: &BigInt) -> BigInt {
+        RemDiv::rem(self, other).unwrap()
+    }
+}
+impl DivAssign<&BigInt> for BigInt {
+    fn div_assign(&mut self, other: &BigInt) {
+        *self = &*self / other;
+    }
+}
+impl Div<&BigInt> for &BigInt {
+    type Output = BigInt;
+    fn div(self, other: &BigInt) -> BigInt {
+        RemDiv::div(self, other).unwrap()
     }
 }
