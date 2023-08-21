83beafac7e4e3c4e1a7860c63f9a9fcb638dccb0
twiby <t.balenbois@gmail.com>
2023-08-21 16:05:52 +0200

    remove useless macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,31 +1,3 @@
-/// macro that allows easy construction of a BigUint from any type T for
-/// which From<T> is implemented. Particularly useful for base 10 digit
-/// string: `let uint = biguint!["123456789101112131415"];`
-#[macro_export]
-macro_rules! biguint {
-    ( $( $x:expr ),* ) => {
-        {
-            $(
-                BigUint::<T>::from($x)
-            )*
-        }
-    };
-}
-
-/// macro that allows easy construction of a BigInt from any type T for
-/// which From<T> is implemented. Particularly useful for base 10 digit
-/// string: `let uint = biguint!["-123456789101112131415"];`
-#[macro_export]
-macro_rules! bigint {
-    ( $( $x:expr ),* ) => {
-        {
-            $(
-                BigInt::from($x)
-            )*
-        }
-    };
-}
-
 /// macro that builds test function from a generic input function, running
 /// it on available Digit types: u32, u64
 #[macro_export]
